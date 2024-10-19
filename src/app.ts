5b23e1d53e88cb8742c7ad1d1787ea29c851538e
f3gomes <fgomesdeluna@gmail.com>
2024-10-19 17:21:25 -0300

    refactor: remove unused db connect

--- src/app.ts
+++ src/app.ts
@@ -8,8 +8,6 @@ const PORT = process.env.PORT || 9000;
 app.use(cors());
 app.use(express.json());
 
-// main();
-
 app.get("/", (_req: Request, res: Response) => {
   res.json({
     message: "API is on!",
