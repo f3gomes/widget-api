8cb1d6082fe5f35ffd4431eddb5886d39ac29b37
f3gomes <fgomesdeluna@gmail.com>
2024-10-19 14:36:32 -0300

    feat: connect database

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
+import { connectToDB } from "./config/database";
 
 export const app: Application = express();
 const PORT = process.env.PORT || 9000;
 
 app.use(cors());
 app.use(express.json());
+
+connectToDB();
 
 app.get("/", (_req: Request, res: Response) => {
   res.json({
