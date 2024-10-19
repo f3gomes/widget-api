c04fe99ecbc8c93067ade14da08f3dc3cc6fccd0
f3gomes <fgomesdeluna@gmail.com>
2024-10-19 14:31:29 -0300

    feat: hello route

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+
+export const app: Application = express();
+const PORT = process.env.PORT || 9000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_req: Request, res: Response) => {
+  res.json({
+    message: "API is on!",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
