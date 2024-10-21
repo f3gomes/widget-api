0c207dd9d7bff811c9f24102fd5c7c2c1db3bd35
f3gomes <fgomesdeluna@gmail.com>
2024-10-21 11:25:09 -0300

    db: remove old migrations

--- prisma/migrations/20241019194616_feedback_model/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Feedback" (
-    "id" TEXT NOT NULL,
-    "type" TEXT NOT NULL,
-    "comment" TEXT NOT NULL,
-    "screenshotUrl" TEXT NOT NULL,
-
-    CONSTRAINT "Feedback_pkey" PRIMARY KEY ("id")
-);
--- prisma/migrations/20241020134541_update_feedback_model/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Feedback" ALTER COLUMN "screenshotUrl" DROP NOT NULL;
